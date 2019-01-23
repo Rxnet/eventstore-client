3d7929898ca5b114bbc1e9f8e236424e313707e6
Jérémy Marodon <marodon.jeremy@gmail.com>
2019-01-23 11:13:30 +0100

    :art: Improve examples style

--- examples/read.php
+++ examples/read.php
@@ -8,10 +8,10 @@ require __DIR__.'/../vendor/autoload.php';
 $eventStore = new \Rxnet\EventStore\EventStore();
 $eventStore->connect()
     ->subscribe(function() use ($eventStore) {
-        echo "connected \n";
+        echo "connected".PHP_EOL;
         $eventStore->readEvent('domain-test.fr', 0)
             ->subscribe(function (\Rxnet\EventStore\EventRecord $record) {
-                echo "received {$record->getId()}  {$record->getNumber()}@{$record->getStreamId()} {$record->getType()} created at {$record->getCreated()->format('c')}\n";
+                echo "received {$record->getId()}  {$record->getNumber()}@{$record->getStreamId()} {$record->getType()} created at {$record->getCreated()->format('c')}".PHP_EOL;
             });
     }, function (\Exception $e) {
         echo $e->getMessage();
