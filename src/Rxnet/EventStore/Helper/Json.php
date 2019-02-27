0dc603c349396d0e455244b9fb3fd543fc7f4761
Jérémy Marodon <marodon.jeremy@gmail.com>
2019-02-27 14:21:41 +0100

    :page_facing_up: Add license header

--- src/Rxnet/EventStore/Helper/Json.php
+++ src/Rxnet/EventStore/Helper/Json.php
@@ -2,6 +2,12 @@
 
 declare(strict_types=1);
 
+/*
+ * This file is part of the RxNET project.
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Rxnet\EventStore\Helper;
 
 use Rxnet\EventStore\Exception\JsonException;
